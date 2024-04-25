c2dc31581bba668e6c7257599b991237e2ababc3
danapc11 <danapc06@gmail.com>
2024-04-25 14:53:25 -0700

    Initial Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello React Native!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 5,
+    backgroundColor: '#86469C',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
